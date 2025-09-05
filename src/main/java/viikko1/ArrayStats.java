73fa97d7d2f8911bd2bbdd3baea561cddbcde697
AlenaAbaseva <abasevaa@gmail.com>
2025-09-05 11:33:15 +0300

    Abaseva/Viikkotehtava2

--- src/main/java/viikko1/ArrayStats.java
+++ src/main/java/viikko1/ArrayStats.java
@@ -2,11 +2,29 @@ package viikko1;
 
 public class ArrayStats {
     public static double average(int[] arr) {
-        // TODO
-        return 0.0;
+        if (arr.length == 0) {
+            return 0.0;
+        }
+
+        double sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+
+        return sum / arr.length;
     }
+    
     public static int max(int[] arr) {
-        // TODO
-        return Integer.MIN_VALUE;
+        if (arr.length == 0) {
+            return Integer.MIN_VALUE;
+        }
+
+        int maxVal = arr[0];
+        for (int num : arr) {
+            if (num > maxVal) {
+                maxVal = num;
+            }
+        }
+        return maxVal;
     }
 }
--- src/main/java/viikko1/SortThree.java
+++ src/main/java/viikko1/SortThree.java
@@ -2,7 +2,11 @@ package viikko1;
 
 public class SortThree {
     public static String ascending(int a, int b, int c) {
-        // TODO
-        return null;
+        int min, mid, max;
+        min = Math.min(a, Math.min(b, c));
+        max = Math.max(a, Math.max(b, c));
+        mid = a+b+c-min-max;
+
+        return min+ "," +mid+ "," +max;
     }
 }
